25a5a7c1c818efe3db002c311621aca0725d5f74
Sebastian Cejas <seba171018@hotmail.com>
2021-10-29 15:07:43 -0300

    Se agrega proyecto hello world en C

---
+++ Hello_C/cmake-build-debug/CMakeFiles/Hello_C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hello_C.dir/main.c.obj"
+  "Hello_C.exe"
+  "Hello_C.exe.manifest"
+  "Hello_C.lib"
+  "Hello_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Hello_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

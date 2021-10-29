5aa61c2cb584e26ba7032882b2ebbc2b66a487c9
Sebastian Cejas <seba171018@hotmail.com>
2021-10-28 23:59:26 -0300

    Se agrega proyecto hello world en php

---
+++ Hello_PHP/index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title> Hello World En PHP</title>
+        
+    </head>
+    <body>
+        <p>Capacitacion platzi 2021</p>
+        <?php
+        function printHello (){
+            echo "Hello World in PHP!";
+        }
+        printHello();
+        ?>
+      
+    </body>
+</html>
